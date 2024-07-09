1605e072e8c0a03b651f069fbb07bea17be438bc
AdrianDataware <a.guevara@dataware.com.mx>
2024-07-08 20:50:22 -0600

    feat: Add IZebraPrinterServiceFactory interface and implement it in ZebraPrinterServiceFactory class

---
+++ Interfaces/IZebraPrinterServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZebraPrinterLibrary.Interfaces
+{
+    public interface IZebraPrinterServiceFactory
+    {
+        IZebraPrinterService Create(string ip, int port);
+    }
+}
---
+++ Services/ZebraPrinterServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZebraPrinterLibrary.Interfaces;
+
+namespace ZebraPrinterLibrary.Services
+{
+    public class ZebraPrinterServiceFactory : IZebraPrinterServiceFactory
+    {
+        public IZebraPrinterService Create(string ip, int port)
+        {
+            return new ZebraPrinterService(ip, port);
+        }
+    }
+}
